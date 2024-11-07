cd1a2bb63ba7d1bc05425e68b48396d945353d67
Gabriel Covalski <gab.covalski@gmail.com>
2024-11-06 22:49:00 -0300

    Recuperação de Dados ao Iniciar
    Ao iniciar, o programa lê os arquivos de registro, os atribui a um vetor com espaço alocado dinâmicamente para uso posterior dos dados dentro do programa

--- main.c
+++ main.c
@@ -1,15 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//É óbvio que a professora queria de um jeito, então eu tentei seguir o padrão, mas eu deixei as variáveis de um jeito que >>>>eu<<<<< acho mais bonito e original;
-
-typedef struct Livro{
-	int codigo;
-	char titulo[30], autor[15], genero[15];
-	int Status; // 1-Disponível, 2-Reservado, 3-Emprestado
-	int numReservas, quTotal;
-}Livro;
-
 typedef struct Leitor{
 	int codigo;
 	char nome[20], email[25];
@@ -22,16 +13,24 @@ typedef struct Funcionario{
 	int totalEmp, totalDev;
 }Funcionario;
 
+
+typedef struct Livro{
+	int codigo;
+	char titulo[30], autor[15], genero[15];
+	int Status; // 1-Disponível, 2-Reservado, 3-Emprestado
+	int numReservas, quTotal;
+}Livro;
+
 typedef struct emprestimo{
 	int codigo, codLivro, codLeitor;
 	char data_emp[20], data_dev[20];
 	int status; //0-ativo 1-finalizado
-}emprestimo;
+}Emprestimo;
 
 typedef struct reserva{
 	int codigo, codLivro, codLeitor;
 	char data_reserva[20];
-}
+}Reserva;
 
 
 
@@ -48,12 +47,11 @@ void Cadastros(){
 	printf("\n\nDigite um número: "); scanf("%d",&tipo); while ((getchar()) != '\n');
 	
 	switch(tipo){
-		case 1:
+		case 1: //Escolheu Cadastrar Leitor
 			system("clear");
 			Leitor *novoLeitor=(Leitor*)malloc(sizeof(Leitor));
 			novoLeitor->qtdeEmp=0;
 			novoLeitor->histMulta=0;
-			//O sistema de  atribuição de código que vale para todos os typedef que eu vou fazer vai ter que ser dinâmico, o que significa que depende de vocêKKKKKKKK
 			
 			//Nome
 			printf("\n-----------------------------------------------------------------------");
@@ -67,10 +65,24 @@ void Cadastros(){
 			printf("\n\nDigite: "); fgets(novoLeitor->email, 15, stdin);
 			
 			return;
-		case 2:
+		case 2: //Escolheu Cadastrar Funcionario
+			system("clear");
+			Funcionario *novoFunc=(Funcionario*)malloc(sizeof(Funcionario));
+			novoFunc->totalEmp=0;
+			novoFunc->totalDev=0;
 			
+			//Nome
+			printf("\n-----------------------------------------------------------------------");
+			printf("\nInforme o seu nome (20 caracteres max.)");
+			printf("\n-----------------------------------------------------------------------");
+			printf("\n\nDigite: ");fgets(novoFunc->nome, 20, stdin);
+			//Cargo
+			printf("\n-----------------------------------------------------------------------");
+			printf("\nInforme o cargo (15 caracteres max.)");
+			printf("\n-----------------------------------------------------------------------");
+			printf("\n\nDigite: "); fgets(novoFunc->cargo, 15, stdin);
 			break;
-		case 3:
+		case 3: //Escolheu Sair
 
 			return;
 	} Cadastros();
@@ -78,7 +90,7 @@ void Cadastros(){
 
 //Funções gerais/básicas do programa
 
-void Inicio(){ //EXISTE SISTEMA DE CONTA!!! Vai ter que existir MenuLeitor(); MenuFuncionario(); futuramente
+void InUser(){ //EXISTE SISTEMA DE CONTA!!! Vai ter que existir MenuLeitor(); MenuFuncionario(); futuramente
 	int esc;
 	
 	printf("\n-----------------------------------------------------------------------");
@@ -95,20 +107,174 @@ void Inicio(){ //EXISTE SISTEMA DE CONTA!!! Vai ter que existir MenuLeitor(); Me
 			break;
 		case 3: //Escolheu Sair
 			return;
-	} Inicio();
+	} InUser();
 }
 
 
+//
+//
+//A PARTIR DAQUI ATÉ O PRÓXIMO PONTO MARCADO, E SOMENTE PARA O RESGATE DE DADOS------------------------------------------
+//
+//
+Reserva *LogReserva(){
+	FILE *poLog = fopen("reservas.txt","r");
+	if(!poLog){
+	poLog = fopen("reservas.txt","w");
+	//Ainda preciso inserir a primeira linha do arquivo novo criado, mas isso e problema para o eu do futuro
+	printf("\n-----------------------------------------------------------------------");
+	printf("\nAVISO: O arquivo de dados 'reservas.txt'nao foi encontrado nesta maquina, entao um novo foi criado.");
+	printf("\n-----------------------------------------------------------------------\n");
+	}
+
+	int linhas;
+	fscanf(poLog,"%d",&linhas);
+	
+	Reserva *liveLog = (Reserva *)malloc((linhas+1)*sizeof(Reserva));
+	int i;	
+	for(i=1;i<=linhas;i++){
+		fscanf(poLog,"%d",&liveLog[i].codigo);
+		fscanf(poLog,"%d",&liveLog[i].codLivro);
+		fscanf(poLog,"%d",&liveLog[i].codLeitor);
+		fgets(liveLog[i].data_reserva,20,poLog);
+	}
+	fclose(poLog);
+	return liveLog;
+}
+Emprestimo *LogEmprestimo(){
+	FILE *poLog = fopen("emprestimos.txt","r");
+	if(!poLog){
+	poLog = fopen("emprestimos.txt","w");
+	//Ainda preciso inserir a primeira linha do arquivo novo criado, mas isso e problema para o eu do futuro
+	printf("\n-----------------------------------------------------------------------");
+	printf("\nAVISO: O arquivo de dados 'emprestimos.txt'nao foi encontrado nesta maquina, entao um novo foi criado.");
+	printf("\n-----------------------------------------------------------------------\n");
+	}
+
+	int linhas;
+	fscanf(poLog,"%d",&linhas);
+	
+	Emprestimo *liveLog = (Emprestimo *)malloc((linhas+1)*sizeof(Emprestimo));
+	
+	int i;	
+	for(i=1;i<=linhas;i++){
+		fscanf(poLog,"%d",&liveLog[i].codigo);
+		fscanf(poLog,"%d",&liveLog[i].codLivro);
+		fscanf(poLog,"%d",&liveLog[i].codLeitor);
+		fgets(liveLog[i].data_emp,20,poLog);
+		fgets(liveLog[i].data_dev,20,poLog);
+		fscanf(poLog,"%d",&liveLog[i].status);
+	}
+	fclose(poLog);
+	return liveLog;
+}
+Livro *LogLivro(){
+	FILE *poLog = fopen("livros.txt","r");
+	if(!poLog){
+	poLog = fopen("livros.txt","w");
+	//Ainda preciso inserir a primeira linha do arquivo novo criado, mas isso e problema para o eu do futuro
+	printf("\n-----------------------------------------------------------------------");
+	printf("\nAVISO: O arquivo de dados 'livros.txt'nao foi encontrado nesta maquina, entao um novo foi criado.");
+	printf("\n-----------------------------------------------------------------------\n");
+	}
+
+	int linhas;
+	fscanf(poLog,"%d",&linhas);
+	
+	Livro *liveLog = (Livro *)malloc((linhas+1)*sizeof(Livro));
+	
+	int i;	
+	for(i=1;i<=linhas;i++){
+		fscanf(poLog,"%d",&liveLog[i].codigo);
+		fgets(liveLog[i].titulo,20,poLog);
+		fgets(liveLog[i].autor,15,poLog);
+		fgets(liveLog[i].genero,15,poLog);
+		fscanf(poLog,"%d",&liveLog[i].Status);
+		fscanf(poLog,"%d",&liveLog[i].numReservas);
+		fscanf(poLog,"%d",&liveLog[i].quTotal);
+	}
+	fclose(poLog);
+	return liveLog;
+}
+
+Funcionario *LogFuncionario(){
+	FILE *poLog = fopen("funcionarios.txt","r");
+	if(!poLog){
+	poLog = fopen("funcionarios.txt","w");
+	//Ainda preciso inserir a primeira linha do arquivo novo criado, mas isso e problema para o eu do futuro
+	printf("\n-----------------------------------------------------------------------");
+	printf("\nAVISO: O arquivo de dados 'funcionarios.txt'nao foi encontrado nesta maquina, entao um novo foi criado.");
+	printf("\n-----------------------------------------------------------------------\n");
+	}
+
+	int linhas;
+	fscanf(poLog,"%d",&linhas);
+	
+	Funcionario *liveLog = (Funcionario *)malloc((linhas+1)*sizeof(Funcionario));
+	
+	int i;	
+	for(i=1;i<=linhas;i++){
+		fscanf(poLog,"%d",&liveLog[i].codigo);
+		fgets(liveLog[i].nome,20,poLog);
+		fgets(liveLog[i].cargo,15,poLog);
+		fscanf(poLog,"%d",&liveLog[i].totalEmp);
+		fscanf(poLog,"%d",&liveLog[i].totalDev);
+	}
+	fclose(poLog);
+	return liveLog;
+}
+
+Leitor *LogLeitor(){
+	FILE *poLog = fopen("leitores.txt","r");
+	if(!poLog){
+	poLog = fopen("leitores.txt","w");
+	//Ainda preciso inserir a primeira linha do arquivo novo criado, mas isso e problema para o eu do futuro
+	printf("\n-----------------------------------------------------------------------");
+	printf("\nAVISO: O arquivo de dados 'leitores.txt'nao foi encontrado nesta maquina, entao um novo foi criado.");
+	printf("\n-----------------------------------------------------------------------\n");
+	}
+	
+	int linhas;
+	fscanf(poLog,"%d",&linhas);
+	
+	Leitor *liveLog = (Leitor *)malloc((linhas+1)*sizeof(Leitor));
+	
+	int i;	
+	for(i=1;i<=linhas;i++){
+		fscanf(poLog,"%d",&liveLog[i].codigo);
+		fgets(liveLog[i].nome,20,poLog);
+		fgets(liveLog[i].email,15,poLog);
+		fscanf(poLog,"%d",&liveLog[i].qtdeEmp);
+		fscanf(poLog,"%d",&liveLog[i].histMulta);
+	}
+	fclose(poLog);
+	return liveLog;
+}
 
 
+//
+//
+//FIM DO RESGATE DE DADOS--------------------------------------------------------------------------------------------
+//
+//
 
 
 int main(){ //Vamo fazer igual foi no jogo, onde a main só faz chamar função
 	printf("-----------------------------------------------------------------------");	
-	printf("\nBem vindo(a) ao acesso digital da Grande Biblioteca Publica de Castro!"); //Interface bonitinha
-	printf("\n-----------------------------------------------------------------------\n\n");
+	printf("\nBem vindo(a) ao acesso digital da Grande Biblioteca Publica de Castro!");
+	printf("\n-----------------------------------------------------------------------\n");
 	
-	Inicio();
+	//Leitura do banco de dados de cada arquivo
+	//Acabei fazendo função separada para cada para poder retornar os ponteiros de cada, e usar quando necessário
+	
+	Leitor * LLL/*Live Log Leitor*/ = LogLeitor();
+	Funcionario * LLF /*Live Log Funcionario*/= LogFuncionario();
+	Livro * LLI /*Live Log lIvro*/= LogLivro();
+	Emprestimo * LLE /*Live Log Emprestimo*/= LogEmprestimo();
+	Reserva * LLR /*Live Log Reserva*/= LogReserva();
+	
+	//
+	
+	InUser();
 	
 	printf("\n-----------------------------------------------------------------------");
 	printf("\nAgradecemos a preferencia! Volte sempre.");
