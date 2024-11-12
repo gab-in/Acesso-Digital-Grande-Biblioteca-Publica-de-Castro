f6ec9e2d2c747aad5c719a7f3e50d8c7fcce47d0
Daniel Dwulatka <138732225+DanhDie@users.noreply.github.com>
2024-11-12 19:00:12 -0300

    Cadastro implementado

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 #include<string.h>
 #include<ctype.h>
 
@@ -47,44 +47,7 @@ void inputStr(char *str,int tamanho);
 
 //Funções da minha parte
 
-void Cadastros(Leitor *LLL, Funcionario *LLF, Livro *LLI){
-	int tipo;
-
-	printf("\n-----------------------------------------------------------------------");
-	printf("\nQue tipo de cadastro? Escolha uma das opcoes listadas abaixo");
-	printf("\n-----------------------------------------------------------------------");
-	printf("\n(1) Leitores | (2) Funcionarios | (3) Livro | (4) Voltar");
-	printf("\n\nDigite um número: "); scanf("%d",&tipo); while ((getchar()) != '\n');
-	
-	switch(tipo){
-		case 1: //Escolheu Cadastrar Leitor
-			
-			
-			return;
-		case 2: //Escolheu Cadastrar Funcionario
-			system("clear");
-			Funcionario *novoFunc=(Funcionario*)malloc(sizeof(Funcionario));
-			novoFunc->totalEmp=0;
-			novoFunc->totalDev=0;
-			
-			//Nome
-			printf("\n-----------------------------------------------------------------------");
-			printf("\nInforme o seu nome (20 caracteres max.)");
-			printf("\n-----------------------------------------------------------------------");
-			printf("\n\nDigite: ");inputStr(novoFunc->nome,20);
-			//Cargo
-			printf("\n-----------------------------------------------------------------------");
-			printf("\nInforme o cargo (15 caracteres max.)");
-			printf("\n-----------------------------------------------------------------------");
-			printf("\n\nDigite: ");inputStr(novoFunc->cargo,15);
-			break;
-		case 3:
-
-			break;
-		case 4:
-			return;
-	} Cadastros(LLL,LLF,LLI);
-}
+void Cadastros(Leitor *LLL, Funcionario *LLF, Livro *LLI);
 
 //Funções gerais/básicas do programa
 
@@ -92,133 +55,19 @@ void menuOp();
 
 void menuAdm(Leitor *LLL, Funcionario *LLF, Livro *LLI, Emprestimo *LLE, Reserva *LLR);
 
-//
-//
-//A PARTIR DAQUI ATÉ O PRÓXIMO PONTO MARCADO, E SOMENTE PARA O RESGATE DE DADOS------------------------------------------
-//
-//
-Reserva *LogReserva(){
-	FILE *poLog = fopen("reservas.txt","r");
-	int linhas;
-	fscanf(poLog,"%d",&linhas);
-	if(linhas==0){ 
-		printf("\n-----------------------------------------------------------------------");
-		printf("\nAVISO: O arquivo de dados 'reservas.txt' nao foi encontrado nesta maquina, entao um novo foi criado.");
-		printf("\n-----------------------------------------------------------------------\n");
-		fprintf(poLog,"0");
-	}
-	
-	Reserva *liveLog = (Reserva *)malloc((linhas+1)*sizeof(Reserva));
-	
-	int i;	
-	for(i=1;i<=linhas;i++){
-		fscanf(poLog,"%d %d %d %s",&liveLog[i].codigo, &liveLog[i].codLivro, &liveLog[i].codLeitor, liveLog[i].data_reserva);
-	}
-	
-	fclose(poLog);
-	return liveLog;
-}
+//leitura de dados
+Reserva *LogReserva();
 
-Emprestimo *LogEmprestimo(){
-	FILE *poLog = fopen("emprestimos.txt","r");
-	int linhas;
-	fscanf(poLog,"%d",&linhas);
-	if(linhas==0){ 
-		printf("\n-----------------------------------------------------------------------");
-		printf("\nAVISO: O arquivo de dados 'emprestimos.txt' nao foi encontrado nesta maquina, entao um novo foi criado.");
-		printf("\n-----------------------------------------------------------------------\n");
-		fprintf(poLog,"0");
-	}
-	
-	Emprestimo *liveLog = (Emprestimo *)malloc((linhas+1)*sizeof(Emprestimo));
-	
-	int i;	
-	for(i=1;i<=linhas;i++){
-		fscanf(poLog,"%d %d %d %s %s %d",&liveLog[i].codigo, &liveLog[i].codLivro, &liveLog[i].codLeitor, liveLog[i].data_emp, liveLog[i].data_dev, &liveLog[i].status);
-	}
-	
-	fclose(poLog);
-	return liveLog;
-}
+Emprestimo *LogEmprestimo();
 
-Livro *LogLivro(){
-	FILE *poLog = fopen("livros.txt","a+");
-	int linhas;
-	fscanf(poLog,"%d",&linhas);
-	if(linhas==0){ 
-		printf("\n-----------------------------------------------------------------------");
-		printf("\nAVISO: O arquivo de dados 'livros.txt' nao foi encontrado nesta maquina, entao um novo foi criado.");
-		printf("\n-----------------------------------------------------------------------\n");
-		fprintf(poLog,"0");
-	}
-	
-	Livro *liveLog = (Livro *)malloc((linhas+1)*sizeof(Livro));
-	
-	int i;	
-	for(i=1;i<=linhas;i++){
-		fscanf(poLog,"%d %s %s %s %d %d %d",&liveLog[i].codigo, liveLog[i].titulo, liveLog[i].autor, liveLog[i].genero, &liveLog[i].Status, &liveLog[i].numReservas, &liveLog[i].quTotal);
-	}
-	
-	fclose(poLog);
-	return liveLog;
-}
+Livro *LogLivro();
 
-Funcionario *LogFuncionario(){
-	FILE *poLog = fopen("funcionarios.txt","a+");
-	int linhas;
-	fscanf(poLog,"%d",&linhas);
-	if(linhas==0){ 
-		printf("\n-----------------------------------------------------------------------");
-		printf("\nAVISO: O arquivo de dados 'funcionarios.txt' nao foi encontrado nesta maquina, entao um novo foi criado.");
-		printf("\n-----------------------------------------------------------------------\n");
-		fprintf(poLog,"0");
-	}	
-	
-	Funcionario *liveLog = (Funcionario *)malloc((linhas+1)*sizeof(Funcionario));
-	
-	int i;	
-	for(i=1;i<=linhas;i++){
-		fscanf(poLog,"%d %s %s %d %d",&liveLog[i].codigo,liveLog[i].nome,liveLog[i].cargo,&liveLog[i].totalEmp, &liveLog[i].totalDev);
-	}
-	
-	fclose(poLog);
-	return liveLog;
-}
+Funcionario *LogFuncionario();
 
-Leitor *LogLeitor(){
-	FILE *poLog = fopen("leitores.txt","a+");
-	int linhas;
-	
-	fscanf(poLog,"%d",&linhas);
-	if(linhas==0){ 
-		printf("\n-----------------------------------------------------------------------");
-		printf("\nAVISO: O arquivo de dados 'leitores.txt' nao foi encontrado nesta maquina, entao um novo foi criado.");
-		printf("\n-----------------------------------------------------------------------\n");
-		fprintf(poLog,"0");
-	}
-	
-	Leitor *liveLog = (Leitor *)malloc((linhas+1)*sizeof(Leitor));
-	int i;	
-	liveLog[0].codigo=linhas;
-	printf("\nPrimeiro coisa do vetor: %d",liveLog[0].codigo);
-	for(i=1;i<=linhas;i++){
-		fscanf(poLog,"%d %s %s %d %d",&liveLog[i].codigo, liveLog[i].nome, liveLog[i].email, &liveLog[i].qtdeEmp, &liveLog[i].histMulta);
-	}
-	
-	for(i=1;i<=linhas;i++){
-		printf("%d %s %s %d %d",liveLog[i].codigo, liveLog[i].nome, liveLog[i].email, liveLog[i].qtdeEmp, liveLog[i].histMulta);
-	}
-	fclose(poLog);
-	return liveLog;
-}
+Leitor *LogLeitor();
 
 void registrar();
 
-//
-//
-//FIM DO RESGATE DE DADOS--------------------------------------------------------------------------------------------
-//
-//
 
 
 int main(){ //Vamo fazer igual foi no jogo, onde a main só faz chamar função
@@ -248,7 +97,7 @@ int main(){ //Vamo fazer igual foi no jogo, onde a main só faz chamar função
 		default:
 			break;
 	}
-	
+
 	printf("\n-----------------------------------------------------------------------");
 	printf("\nAgradecemos a preferencia! Volte sempre.");
 	printf("\n-----------------------------------------------------------------------");
@@ -298,6 +147,7 @@ int login(Funcionario *user){
 	return 0;
 	
 }
+
 void formatarString(char *s,int op){
 	int i,inicial=1;
 
@@ -386,4 +236,254 @@ void menuAdm(Leitor *LLL, Funcionario *LLF, Livro *LLI, Emprestimo *LLE, Reserva
 	}menuAdm(LLL,LLF,LLI,LLE,LLR);
 }
 
-void registrar(){}
+void Cadastros(Leitor *LLL, Funcionario *LLF, Livro *LLI){
+	int tipo;
+
+	printf("\n-----------------------------------------------------------------------");
+	printf("\nQue tipo de cadastro? Escolha uma das opcoes listadas abaixo");
+	printf("\n-----------------------------------------------------------------------");
+	printf("\n(1) Leitores | (2) Funcionarios | (3) Livro | (4) Voltar");
+	printf("\n\nDigite um numero: "); scanf("%d",&tipo); while ((getchar()) != '\n');
+	
+	switch(tipo){
+		case 1: //Escolheu Cadastrar Leitor
+			system("clear");
+			char nome[20],email[25];
+			Leitor *novoLeitor=(Leitor*)malloc(sizeof(Leitor));
+			novoLeitor->codigo=((LLL[0].codigo)+1);
+			novoLeitor->qtdeEmp=0;
+			novoLeitor->histMulta=0;
+			
+			//Nome
+			printf("\n-----------------------------------------------------------------------");
+			printf("\nInforme o seu nome (20 caracteres max.)");
+			printf("\n-----------------------------------------------------------------------");
+			printf("\n\nDigite: ");inputStr(nome,20);
+			formatarString(nome,1);
+			strcpy(novoLeitor->nome,nome);
+			//email
+			printf("\n-----------------------------------------------------------------------");
+			printf("\nInforme o email (25 caracteres max.)");
+			printf("\n-----------------------------------------------------------------------");
+			printf("\n\nDigite: ");inputStr(email,25);
+			strcpy(novoLeitor->email,email);
+
+			realloc(LLL,((LLL[0].codigo)+1)*(sizeof(Leitor)));
+
+			LLL[(LLL[0].codigo)+1]=*novoLeitor;
+			LLL[0].codigo+=1;
+			
+			//tirar dps
+			printf("Leitores: ");			
+			printf("\nPrimeiro coisa do vetor: %d",LLL[0].codigo);
+			for(int i=1;i<=LLL[0].codigo;i++){
+				printf("\n%d %s %s %d %d",LLL[i].codigo, LLL[i].nome, LLL[i].email, LLL[i].qtdeEmp, LLL[i].histMulta);
+			}
+			//
+	
+
+			
+			return;
+		case 2: //Escolheu Cadastrar Funcionario
+			system("clear");
+			char n[20];
+			Funcionario *novoFunc=(Funcionario*)malloc(sizeof(Funcionario));
+			novoFunc->codigo=((LLF[0].codigo)+1);
+			novoFunc->totalEmp=0;
+			novoFunc->totalDev=0;
+			
+			//Nome
+			printf("\n-----------------------------------------------------------------------");
+			printf("\nInforme o seu nome (20 caracteres max.)");
+			printf("\n-----------------------------------------------------------------------");
+			printf("\n\nDigite: ");inputStr(n,20);
+			formatarString(n,1);
+			strcpy(novoFunc->nome,n);
+			//Cargo
+			printf("\n-----------------------------------------------------------------------");
+			printf("\nInforme o cargo (15 caracteres max.)");
+			printf("\n-----------------------------------------------------------------------");
+			printf("\n\nDigite: ");inputStr(novoFunc->cargo,15);
+
+			realloc(LLF,((LLF[0].codigo)+1)*(sizeof(Funcionario)));
+			LLF[(LLF[0].codigo)+1]=*novoFunc;
+			LLF[0].codigo+=1;
+
+			//tirar dps
+			printf("Funcionarios: ");			
+			printf("\nPrimeiro coisa do vetor: %d",LLF[0].codigo);
+			for(int i=1;i<=LLF[0].codigo;i++){
+				printf("\n%d %s %s %d %d",LLF[i].codigo, LLF[i].nome, LLF[i].cargo, LLF[i].totalEmp, LLF[i].totalDev);
+			}
+			
+			break;
+		case 3: //escolheu livro
+			system("clear");
+			char titulo[30],autor[20], genero[15];
+			Livro *novoLivro=(Livro*)malloc(sizeof(Livro));
+			novoLivro->codigo=((LLI[0].codigo)+1);
+			novoLivro->Status=1;
+			novoLivro->numReservas=0;
+			
+			
+			//titulo
+			printf("\n-----------------------------------------------------------------------");
+			printf("\nInforme o titulo do livro (30 caracteres max.)");
+			printf("\n-----------------------------------------------------------------------");
+			printf("\n\nDigite: ");inputStr(titulo,30);
+			strcpy(novoLivro->titulo,titulo);
+			//autor
+			printf("\n-----------------------------------------------------------------------");
+			printf("\nInforme o autor (20 caracteres max.)");
+			printf("\n-----------------------------------------------------------------------");
+			printf("\n\nDigite: ");inputStr(autor,20);
+			strcpy(novoLivro->autor,autor);
+			//genero
+			printf("\n-----------------------------------------------------------------------");
+			printf("\nInforme o genero (15 caracteres max.)");
+			printf("\n-----------------------------------------------------------------------");
+			printf("\n\nDigite: ");inputStr(genero,15);
+			strcpy(novoLivro->genero,genero);
+			//quantidade
+			printf("\n-----------------------------------------------------------------------");
+			printf("\nInforme quantos livros tem");
+			printf("\n-----------------------------------------------------------------------");
+			printf("\n\nDigite: ");
+			scanf("%d",&novoLivro->quTotal);
+
+			realloc(LLI,((LLI[0].codigo)+1)*(sizeof(Livro)));
+			LLI[(LLI[0].codigo)+1]=*novoLivro;
+			LLI[0].codigo+=1;
+
+			//tirar dps
+			printf("Livros: ");			
+			printf("\nPrimeiro coisa do vetor: %d",LLI[0].codigo);
+			for(int i=1;i<=LLI[0].codigo;i++){
+				printf("\n%d %s %s %s %d %d %d",LLI[i].codigo, LLI[i].titulo, LLI[i].autor, LLI[i].genero, LLI[i].Status,LLI[i].numReservas, LLI[i].quTotal);
+			}
+
+			break;
+		case 4:
+			return;
+		}
+}
+
+Reserva *LogReserva(){
+	FILE *poLog = fopen("reservas.txt","r");
+	int linhas;
+	fscanf(poLog,"%d",&linhas);
+	if(linhas==0){ 
+		printf("\n-----------------------------------------------------------------------");
+		printf("\nAVISO: O arquivo de dados 'reservas.txt' nao foi encontrado nesta maquina, entao um novo foi criado.");
+		printf("\n-----------------------------------------------------------------------\n");
+		fprintf(poLog,"0");
+	}
+	
+	Reserva *liveLog = (Reserva *)malloc((linhas+1)*sizeof(Reserva));
+	liveLog[0].codigo=linhas;
+	int i;	
+	for(i=1;i<=linhas;i++){
+		fscanf(poLog,"%d %d %d %s",&liveLog[i].codigo, &liveLog[i].codLivro, &liveLog[i].codLeitor, liveLog[i].data_reserva);
+	}
+	
+	fclose(poLog);
+	return liveLog;
+}
+
+Emprestimo *LogEmprestimo(){
+	FILE *poLog = fopen("emprestimos.txt","r");
+	int linhas;
+	fscanf(poLog,"%d",&linhas);
+	if(linhas==0){ 
+		printf("\n-----------------------------------------------------------------------");
+		printf("\nAVISO: O arquivo de dados 'emprestimos.txt' nao foi encontrado nesta maquina, entao um novo foi criado.");
+		printf("\n-----------------------------------------------------------------------\n");
+		fprintf(poLog,"0");
+	}
+	
+	Emprestimo *liveLog = (Emprestimo *)malloc((linhas+1)*sizeof(Emprestimo));
+	liveLog[0].codigo=linhas;
+	int i;	
+	for(i=1;i<=linhas;i++){
+		fscanf(poLog,"%d %d %d %s %s %d",&liveLog[i].codigo, &liveLog[i].codLivro, &liveLog[i].codLeitor, liveLog[i].data_emp, liveLog[i].data_dev, &liveLog[i].status);
+	}
+	
+	fclose(poLog);
+	return liveLog;
+}
+
+Livro *LogLivro(){
+	FILE *poLog = fopen("livros.txt","a+");
+	int linhas;
+	fscanf(poLog,"%d",&linhas);
+	if(linhas==0){ 
+		printf("\n-----------------------------------------------------------------------");
+		printf("\nAVISO: O arquivo de dados 'livros.txt' nao foi encontrado nesta maquina, entao um novo foi criado.");
+		printf("\n-----------------------------------------------------------------------\n");
+		fprintf(poLog,"0");
+	}
+	
+	Livro *liveLog = (Livro *)malloc((linhas+1)*sizeof(Livro));
+	liveLog[0].codigo=linhas;
+	int i;	
+	for(i=1;i<=linhas;i++){
+		fscanf(poLog,"%d %s %s %s %d %d %d",&liveLog[i].codigo, liveLog[i].titulo, liveLog[i].autor, liveLog[i].genero, &liveLog[i].Status, &liveLog[i].numReservas, &liveLog[i].quTotal);
+	}
+	
+	fclose(poLog);
+	return liveLog;
+}
+
+Funcionario *LogFuncionario(){
+	FILE *poLog = fopen("funcionarios.txt","a+");
+	int linhas;
+	fscanf(poLog,"%d",&linhas);
+	if(linhas==0){ 
+		printf("\n-----------------------------------------------------------------------");
+		printf("\nAVISO: O arquivo de dados 'funcionarios.txt' nao foi encontrado nesta maquina, entao um novo foi criado.");
+		printf("\n-----------------------------------------------------------------------\n");
+		fprintf(poLog,"0");
+	}	
+	
+	Funcionario *liveLog = (Funcionario *)malloc((linhas+1)*sizeof(Funcionario));
+	liveLog[0].codigo=linhas;
+	int i;	
+	for(i=1;i<=linhas;i++){
+		fscanf(poLog,"%d %s %s %d %d",&liveLog[i].codigo,liveLog[i].nome,liveLog[i].cargo,&liveLog[i].totalEmp, &liveLog[i].totalDev);
+	}
+	
+	fclose(poLog);
+	return liveLog;
+}
+
+Leitor *LogLeitor(){
+	FILE *poLog = fopen("leitores.txt","a+");
+	int linhas;	
+	fscanf(poLog,"%d",&linhas);
+	if(linhas==0){ 
+		printf("\n-----------------------------------------------------------------------");
+		printf("\nAVISO: O arquivo de dados 'leitores.txt' nao foi encontrado nesta maquina, entao um novo foi criado.");
+		printf("\n-----------------------------------------------------------------------\n");
+		fprintf(poLog,"0");
+	}
+
+	Leitor *liveLog = (Leitor *)malloc((linhas+1)*sizeof(Leitor));
+	int i;	
+	liveLog[0].codigo=linhas;
+	
+	for(i=1;i<=linhas;i++){
+		fscanf(poLog,"%d %s %s %d %d",&liveLog[i].codigo, liveLog[i].nome, liveLog[i].email, &liveLog[i].qtdeEmp, &liveLog[i].histMulta);
+	}
+	//Tirar depois
+	printf("Leitores: ");
+	printf("\nPrimeiro coisa do vetor: %d",liveLog[0].codigo);
+	for(int i=1;i<=linhas;i++){
+		printf("\n%d %s %s %d %d",liveLog[i].codigo, liveLog[i].nome, liveLog[i].email, liveLog[i].qtdeEmp, liveLog[i].histMulta);
+	}
+	fclose(poLog);
+	return liveLog;
+}
+
+void registrar(){
+	return;
+}
